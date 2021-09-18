40dffa7bd6f64b7d524566e242307d0cbbdbc2fd
Viktotija Popovskaja <yanvarskaja89@gmail.com>
2021-09-18 13:19:32 +0800

    исправлены заметки

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -1,26 +1,26 @@
 interface  CrudServiceNote<E> {
     fun add(entity: E) : Note
      fun delete(noteId: Int)
-     fun edit(entity: E)
-     fun get(entity: E)
+     fun edit(noteId: Int, title: String, text: String, entity: E)
+     fun get(entity: E) : Note
      fun getById(noteId: Int)
 }
 
 interface  CrudServiceComment<E> {
     fun createComment(entity: E, noteId: Int) : NoteComment
     fun deleteComment(noteId: Int) : NoteComment
-    fun editComment(entity: E)
-    fun getComment(entity: E)
+    fun editComment(commentId: Int, message: String, entity: E)
+    fun getComment(entity: E) : NoteComment
     fun restoreComment(commentId: Int)
 }
 
-class Note(
+data class Note(
     var title: String,
     val text: String,
     var noteId: Int = 1
 )
 
-class NoteComment(
+data class NoteComment(
     val message: String,
     val commentId: Int,
 )
@@ -51,30 +51,34 @@ class NoteService: CrudServiceNote<Note>, CrudServiceComment<NoteComment> {
         return comments.last()
     }
 
-    override fun edit(note: Note) {
-    for ((index, note) in notes.withIndex())
-        if (note.noteId == notes.indexOf(note)) {
-//           note.title =
-//           note.text =     
+    override fun edit(noteId: Int, title: String, text: String, note: Note) {
+        val note = notes[noteId]
+        if (note != null) {
+            val newNote = note.copy(title = title, text = text)
+            notes[noteId] = newNote
         }
-
-            return
     }
 
-    override fun editComment(entity: NoteComment) {
-        TODO("Not yet implemented")
+    override fun editComment(commentId: Int, message: String, entity: NoteComment) {
+        val comment = comments[commentId]
+        if (comment != null) {
+            val newComment = comment.copy(message = message)
+            comments[commentId] = newComment
+        }
     }
 
-    override fun get(note: Note) {
+    override fun get(note: Note) : Note {
     notes.getOrNull(note.noteId)
+        return note
     }
 
     override fun getById(noteId: Int) {
         notes.getOrNull(noteId)
     }
 
-    override fun getComment(comment: NoteComment) {
+    override fun getComment(comment: NoteComment) : NoteComment {
         comments.getOrNull(comment.commentId)
+        return comment
     }
 
     override fun restoreComment(commentId: Int) {
